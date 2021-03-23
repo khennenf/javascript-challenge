217f3783cfc03dff9fff011fb0e8ad35d0ae2e86
Keri <khennenf@yahoo.com>
2021-03-22 21:09:57 -0400

    data set

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,14 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+//Check data import
+console.log(tableData)
+
+//Reference to the table body
+var tbody = d3.select("tbody");
+
+//Console log each object
+tableData.forEach(function(ufoSightings){
+    console.log(ufoSightings);
+})
