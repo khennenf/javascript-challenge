602f696b22ac8d9e3c380fdccba600f9fd190bc8
Keri <khennenf@yahoo.com>
2021-03-22 21:29:01 -0400

    object entries added

--- static/js/app.js
+++ static/js/app.js
@@ -4,12 +4,23 @@ var tableData = data;
 // YOUR CODE HERE!
 
 //Check data import
-console.log(tableData)
+      // console.log(tableData)
 
 //Reference to the table body
 var tbody = d3.select("tbody");
 
 //Console log each object
 tableData.forEach(function(ufoSightings){
-    console.log(ufoSightings);
-})
+    // console.log(ufoSightings);
+});
+
+//Append the table rows
+tableData.forEach(function(ufoSightings) {
+    var row = tbody.append("tr");
+
+
+    Object.entries(ufoSightings).forEach(function([key, value]) {
+        console.log(key, value);
+});
+
+});
