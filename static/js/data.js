60526025f5ca89ac46c020f20d20b6f1f2980190
Keri <khennenf@yahoo.com>
2021-03-25 23:23:59 -0400

    changes

--- static/js/data.js
+++ static/js/data.js
@@ -8,7 +8,7 @@ var data = [{
     comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door."
   },
   {
-    datetime: "1/1/201",
+    datetime: "1/1/2010",
     city: "bonita",
     state: "ca",
     country: "us",
